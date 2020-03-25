e281b1bb3334b727470034fcf019dcbec45b3d5d
Clement Diouloufet <diouloufet@Clements-MacBook-Pro.local>
2020-03-25 11:16:49 +0100

    refactoring code

--- index.js
+++ index.js
@@ -5,35 +5,23 @@ const URL_GLOBAL = "https://covid19.mathdro.id/api"
 const URL_COUNTRIES = "https://covid19.mathdro.id/api/countries"
 const selectCountrieHtml = document.querySelector("#countrie")
 const ulDataHtml = document.querySelector('.data-results')
-const ulGlobalHtml = document.querySelector('.global-data')
+
 
 const europeCode = ["DE", "AT", "BE", "BG", "CY", "HR", "DK", "ES", "EE", "FI", "FR", "GR", "HU", "IE", "IT", "LV", "LT", "MT", "LU", "NL", "PL", "PT", "CZ", "RO", "GB", "SK", "SI", "SE"]
-var asiaCode = ["AF", "AM", "AZ", "BH", "BD", "BT", "BN", "KH", "CN", "CY", "TL", "GE", "HK", "IN", "ID", "IR", "IQ", "IL", "JP", "JO", "KZ", "KW", "KG", "LA", "LB", "MO", "MY", "MV", "MN", "MM", "NP", "OM", "PK", "PH", "QA", "SA", "SG", "LK", "SY", "TJ", "TH", "TR", "TM", "AE", "UZ", "VN", "YE"]
-var africaCode = ["DZ", "AO", "BJ", "BW", "IO", "BF", "BI", "CM", "CV", "CF", "TD", "KM", "DJ", "EG", "GQ", "ER", "ET", "GA", "GH", "GW", "GN", "CI", "KE", "LS", "LR", "MG", "MW", "ML", "MR", "MU", "YT", "MA", "MZ", "NA", "NE", "NG", "RE", "RW", "SH", "ST", "SN", "SC", "SL", "SO", "ZA", "SS", "SD", "SZ", "TZ", "TG", "TN", "UG", "EH", "ZM", "ZW"]
-var northAmericaCode = ["AI", "AG", "AW", "BS", "BB", "BZ", "BM", "CA", "KY", "CR", "CU", "DM", "DO", "SV", "GL", "GD", "GP", "GT", "HT", "HN", "JM", "MQ", "MX", "MS", "NI", "PA", "PR", "KN", "LC", "PM", "VC", "TT", "TC"]
-var southAmericaCode = ["AR", "BO", "BR", "CL", "CO", "EC", "FK", "GF", "GY", "PY", "PE", "SR", "UY", "VE"]
-var oceaniaCode = ["AS", "AU", "CX", "CK", "PF", "GU", "KI", "MH", "NR", "NC", "NZ", "NU", "NF", "MP", "PW", "PG", "PN", "WS", "SB", "TK", "TO", "TV", "UM", "VU", "WF"]
+const asiaCode = ["AF", "AM", "AZ", "BH", "BD", "BT", "BN", "KH", "CN", "CY", "TL", "GE", "HK", "IN", "ID", "IR", "IQ", "IL", "JP", "JO", "KZ", "KW", "KG", "LA", "LB", "MO", "MY", "MV", "MN", "MM", "NP", "OM", "PK", "PH", "QA", "SA", "SG", "LK", "SY", "TJ", "TH", "TR", "TM", "AE", "UZ", "VN", "YE"]
+const africaCode = ["DZ", "AO", "BJ", "BW", "IO", "BF", "BI", "CM", "CV", "CF", "TD", "KM", "DJ", "EG", "GQ", "ER", "ET", "GA", "GH", "GW", "GN", "CI", "KE", "LS", "LR", "MG", "MW", "ML", "MR", "MU", "YT", "MA", "MZ", "NA", "NE", "NG", "RE", "RW", "SH", "ST", "SN", "SC", "SL", "SO", "ZA", "SS", "SD", "SZ", "TZ", "TG", "TN", "UG", "EH", "ZM", "ZW"]
+const northAmericaCode = ["AI", "AG", "AW", "BS", "BB", "BZ", "BM", "CA", "KY", "CR", "CU", "DM", "DO", "SV", "GL", "GD", "GP", "GT", "HT", "HN", "JM", "MQ", "MX", "MS", "NI", "PA", "PR", "KN", "LC", "PM", "VC", "TT", "TC"]
+const southAmericaCode = ["AR", "BO", "BR", "CL", "CO", "EC", "FK", "GF", "GY", "PY", "PE", "SR", "UY", "VE"]
+const oceaniaCode = ["AS", "AU", "CX", "CK", "PF", "GU", "KI", "MH", "NR", "NC", "NZ", "NU", "NF", "MP", "PW", "PG", "PN", "WS", "SB", "TK", "TO", "TV", "UM", "VU", "WF"]
 
 
 // ************************************     FUNCTIONS   ************************************
 
-
-function addOptionElement(content) {
-    let option = document.createElement("option")
-    option.innerHTML = content
-    selectCountrieHtml.appendChild(option)
-}
-
-function addGlobalLiElement(content) {
-    let li = document.createElement("li")
-    li.innerHTML = content
-    ulGlobalHtml.appendChild(li)
-}
-
-function addLiElement(content) {
-    let li = document.createElement("li")
-    li.innerHTML = content
-    ulDataHtml.appendChild(li)
+function addElement(element, content, parentNode) {
+    let ParentNode = document.querySelector(parentNode)
+    let Element = document.createElement(element)
+    Element.innerHTML = content
+    ParentNode.appendChild(Element)
 }
 
 function getLastDataUpdateDate(lastUpdate) {
@@ -63,10 +51,15 @@ fetch(URL_GLOBAL).then(function (response) {
                 const lastUpdate = new Date(data.lastUpdate);
                 const niceDate = getLastDataUpdateDate(lastUpdate);
 
-                addGlobalLiElement(`contaminés : ${confirmed}`)
-                addGlobalLiElement(`morts : ${deaths}`)
-                addGlobalLiElement(`guérris : ${recovered}`)
-                addGlobalLiElement(`Mise à jour des données : ${niceDate}`)
+
+                addElement('li', `contaminés : ${confirmed}`, '.global-data')
+                addElement('li', `morts : ${deaths}`, '.global-data')
+                addElement('li', `guérris : ${recovered}`, '.global-data')
+                addElement('li', `Mise à jour des données : ${niceDate}`, '.global-data')
+
+
+
+
             })
         } else {
             console.log("Mauvaise réponse du réseau sur l'opération fetch global.");
@@ -81,22 +74,17 @@ fetch(URL_COUNTRIES).then(function (response) {
             response.json().then((data) => {
                 console.log(`l'objet JSON du covid countries:`);
                 console.log(data);
+                console.log(`data fetch test :`);
+                console.log(data);
+
+
 
                 let countriesTableau = Object.keys(data.countries).map(function (cle) {
-                    return [String(cle), data.countries[cle]]
-                })
-
-                let iso3Tableau = Object.keys(data.iso3).map(function (cle) {
-                    return [String(cle), data.iso3[cle]];
+                    return [data.countries[cle]]
                 })
 
                 for (let countrie of countriesTableau) {
-                    for (let iso of iso3Tableau) {
-                        if (countrie[1] === iso[0]) {
-                            countrie.push(iso[1])
-                        }
-                    }
-                    addOptionElement(countrie[0])
+                    addElement("option", countrie[0].name, "#countrie")
                 }
 
                 // event listener on select and make fetch on this value
@@ -109,8 +97,8 @@ fetch(URL_COUNTRIES).then(function (response) {
                     }
 
                     for (let countrie of countriesTableau) {
-                        if (selectCountrieHtml.value === countrie[0]) {
-                            var iso = countrie[2]
+                        if (selectCountrieHtml.value === countrie[0].name) {
+                            var iso = countrie[0].iso3
                         }
                     }
 
@@ -126,12 +114,11 @@ fetch(URL_COUNTRIES).then(function (response) {
                                     const lastUpdate = new Date(data.lastUpdate);
                                     const niceDate = getLastDataUpdateDate(lastUpdate);
 
-                                    addLiElement(`Contaminés : ${data.confirmed.value}`)
-                                    addLiElement(`Morts : ${data.deaths.value}`)
-                                    addLiElement(`Guéris : ${data.recovered.value}`)
-                                    addLiElement(`Taux de mortalité : ${mortalityRate}%`)
-                                    addLiElement(`Mise à jour des données : ${niceDate}`)
-
+                                    addElement('li', `Contaminés : ${data.confirmed.value}`, ".data-results")
+                                    addElement('li', `Morts : ${data.deaths.value}`, ".data-results")
+                                    addElement('li', `Guéris : ${data.recovered.value}`, ".data-results")
+                                    addElement('li', `Taux de mortalité : ${mortalityRate}%`, ".data-results")
+                                    addElement('li', `Mise à jour des données : ${niceDate}`, ".data-results")
                                 })
                             } else {
                                 console.log('Mauvaise réponse du réseau');
@@ -151,3 +138,24 @@ fetch(URL_COUNTRIES).then(function (response) {
     .catch(function (error) {
         console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
     });
+
+
+
+
+
+
+fetch("https://covid19.mathdro.id/api/daily/3-23-2020").then(function (response) {
+        if (response.ok) {
+            response.json().then((data) => {
+                console.log("fetch daily :");
+
+                console.log(data);
+
+            })
+        } else {
+            console.log("Mauvaise réponse du réseau sur l'opération fetch global.");
+        }
+    })
+    .catch(function (error) {
+        console.log('Il y a eu un problème avec l\'opération fetch global : ' + error.message);
+    })
